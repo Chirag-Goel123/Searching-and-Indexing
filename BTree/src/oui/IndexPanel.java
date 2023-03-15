6efc789d6c3747b9a4acb88e839829455b8e448b
Chirag-Goel123 <126802800+Chirag-Goel123@users.noreply.github.com>
2023-03-15 20:15:45 +0530

    Add files via upload

--- BTree/src/oui/IndexPanel.java
+++ BTree/src/oui/IndexPanel.java
@@ -32,9 +32,77 @@ public class IndexPanel extends JPanel implements ActionListener{
         lblRows.setFont(new Font("Monospaced",0,50));
         lblRows.setForeground(new Color(255,255,255));
         lblRows.setText("Column:");
+
+        comboIndices=new JComboBox(new String[] {"Select-One",:"Name","UserName","Password"});
+        comboIndices.setFont(new Font("Monospaced",0,35));
+
+        btnCreate=new JButton();
+        btnCreate.setFont(new Font("Monospaced",0,50));
+        btnCreate.addActionListener(this);
+
+        progressBar=new JProgressBar();
+        progressBar.setStringPainted(true);
+        progressBar.setMinimum(0);
+        progressBar.setMaximum(100);
+        progressBar.setFont(new Font("Monospaced",0,50));
+
+        javax.swing.GroupLayout layout=new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+
+        layout.setHorizontalGroup(layout.createParallelGroup().addGroup(
+            layout.createSequentialGroup()
+            .addContainerGap(0, Short.MAX_VALUE)
+            .addComponent(lblRows)
+            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+            .addComponent(comboIndices)
+            .addContainerGap(0,Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup().addContainerGap(0,Short.MAX_VALUE)
+            .addComponent(btnCreate)
+            .addContainerGap(0,Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup().addContainerGap(0,Short.MAX_VALUE)
+            .addComponent(progressBar)
+            .addContainerGap(0,Short.MAX_VALUE)))
+        ));
+
+        layout.setVerticalGroup(layout.createSequentialGroup()
+        .addContainerGap(0, Short.MAX_VALUE)
+
+        .addGroup(layout.createParallelGroup()
+        .addComponent(comboIndices)
+        .addComponent(lblRows)
+        .addGap(50, 50, 50)
+        .addComponent(btnCreate)
+        .addGap(50, 50, 50)
+        .addComponent(progressBar))
+        .addContainerGap(0,Short.MAX_VALUE));
     }
     public void actionPerformed(ActionEvent ae)
     {
+        IndexPanel obj=this;
+        Thread t=new Thread(new Runnable() {
+            @Override
+            public void run()
+            {
+                try{
+                    if(comboIndices.getSelectedIndex()==0)
+                    {
+                        JOptionPane.showMessageDialog(obj, "Please Select a Cloumn");
+                        return;
+                    }
+                    DataManager.CreateIndex(comboIndices.getSelectedItem().toString(),obj);
+                    JOptionPane.showMessageDialog(obj, "Index Created Succesfully");
+                    progressBar.setValue(0);
+                }
+                catch(Exception ex)
+                {
 
+                }
+            }
+        });
+        t.start();
+    }
+    public void updateStatus(int percent)
+    {
+        progressBar.setValue(percent);
     }
 }
--- BTree/src/oui/QueryPanel.java
+++ BTree/src/oui/QueryPanel.java
@@ -1,5 +1,132 @@
 package oui;
 
-public class QueryPanel {
-    
+import javax.swing.JPanel;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle;
+import javax.xml.crypto.Data;
+import javax.naming.directory.SearchResult;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class QueryPanel extends JPanel implements ActionListener{
+    private JLabel lblselQuery;
+    private JComboBox comboBoxQuery;
+    private JLabel lbleql;
+    private JTextField txtQuery;
+    private JButton btnSearch;
+    private JLabel lblStats;
+    private JLabel lblUser;
+
+    public QueryPanel(JTabbedPane tabPane)
+    {
+        tabPane.addTab("Query", this);
+        initComponents();
+    }
+    private void initComponents()
+    {
+        this.setBorder(BorderFactory.createMatteBorder(25, 25, 25, 25, Color.darkGray));
+        this.setBackground(new Color(0,100,0));
+
+        lblselQuery=new JLabel();
+        lblselQuery.setFont(new Font("Monospaced",0,45));
+        lblselQuery.setForeground(new Color(255,255,255));
+        lblselQuery.setText("Select * from Table Where ");
+
+        String[] option={"Name","UserName","Password"};
+        comboBoxQuery=new JComboBox<>(option);
+        comboBoxQuery.setFont(new Font("Monospaced",0,20));
+        comboBoxQuery.setPreferredSize(new Dimension(150,50));
+        
+        lbleql=new JLabel();
+        lbleql.setFont(new Font("Monospaced",0,45));
+        lbleql.setForeground(new Color(255,255,255));
+        lbleql.setText("=");
+
+        txtQuery=new JTextField("");
+        txtQuery.setColumns(10);
+        txtQuery.setFont(new Font("Monospaced",0,36));
+
+        btnSearch=new JButton();
+        btnSearch.setFont(new Font("Monospaced",0,50));
+        btnSearch.setText("Search");
+        btnSearch.addActionListener(this);
+
+        lblStats=new JLabel();
+        lblStats.setFont(new Font("Monospaced",0,25));
+        lblStats.setForeground(new Color(255,255,255));
+        lblStats.setText("Search Method: ##SM##,Time taken: ##TT##,Pages Read: ##PR##");
+
+        lblUser=new JLabel();
+        lblUser.setFont(new Font("Monospaced",0,25));
+        lblUser.setForeground(new Color(255,255,255));
+        lblUser.setText("Id: ##ID##, Name: ##Name##, User Name: ##UN##,Password: ##PWD##");
+
+        javax.swing.GroupLayout layout=new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+
+        layout.setHorizontalGroup(layout.createParallelGroup().addGroup(
+            layout.createSequentialGroup()
+            .addContainerGap(0, Short.MAX_VALUE)
+            .addComponent(lblselQuery)
+            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+            .addComponent(comboBoxQuery)
+            .addContainerGap(0,Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup().addContainerGap(0,Short.MAX_VALUE)
+            .addComponent(lbleql)
+            .addContainerGap(0,Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup().addContainerGap(0,Short.MAX_VALUE)
+            .addComponent(txtQuery)
+            .addContainerGap(0,Short.MAX_VALUE)))
+        ));
+
+        layout.setVerticalGroup(layout.createSequentialGroup()
+        .addContainerGap(0, Short.MAX_VALUE)
+
+        .addGroup(layout.createParallelGroup()
+        .addComponent(lblselQuery)
+        .addComponent(comboBoxQuery)
+        .addGap(50, 50, 50)
+        .addComponent(lbleql)
+        .addGap(50, 50, 50)
+        .addComponent(txtQuery))
+        .addGap(50,50,50)
+        .addComponent(btnSearch)
+        .addGap(50, 50, 50)
+        .addComponent(lblStats)
+        .addGap(50, 50, 50)
+        .addComponent(lblUser)
+        .addContainerGap(0,Short.MAX_VALUE));
+    }
+    public void actionPerformed(ActionEvent ae)
+    {
+       DataManager.Search(comboBoxQuery.getSelectedItem().toString(),txtQuery.getText());
+    }
+
+    public void updateResults(SearchResult result)
+    {
+        if(result.DataRow!=null)
+        {
+            lblUser.setText(
+                "RollNumber: ##RNO## ,Name: ##NAME##, User Name: ##UN##,Password: ##PWD##"
+                .replace("##RNO##", result.DataRow.RollNum +"\n")
+                .replace("##NAME##", result.DataRow.Name +"\n")
+                .replace("##UN##", result.DataRow.UserName +"\n")
+                .replace("##PWD##", result.DataRow.Password +"\n")
+                
+            );
+        }
+        lblStats.setText(
+            "Search Method: ##SM## , Time taken: ##TT##,Pages Read: ##PR##"
+            .replace("##SM##", result.IndexUsed? "Index Seek\n":"Table Scan \n")
+            .replace("##TT##", result.TimeTaken+" ms\n")
+            .replace("##PR##", result.PagesLoaded +"\n")
+        );
+    }
 }
